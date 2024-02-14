fe582172279f18c689c48d444f1d7c10f47f412c
BenasAntanavicius <benaanta@haverford.org>
2024-02-14 10:14:22 -0500

    First commit

---
+++ include/globals.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "main.h"
+
+extern pros::Controller master;
+extern pros::MotorGroup DriveL;
+extern pros::MotorGroup DriveR;
+extern pros::Motor Cata;
+extern pros::Motor Intake;
+extern pros::ADIDigitalOut PneuHoriz;
+extern pros::ADIDigitalOut PneuVert;
---
+++ src/globals.cpp
@@ -0,0 +1,9 @@
+#include "globals.h"
+
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup DriveL({1, 2, 3});
+pros::MotorGroup DriveR({-4, -5, -6});
+pros::Motor Cata({-8});
+pros::Motor Intake({-9});
+pros::ADIDigitalOut PneuHoriz('A', false);
+pros::ADIDigitalOut PneuVert('B', false);
